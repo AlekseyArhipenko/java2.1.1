18f5a49d09419fa80517a075f5343fac7743af59
Aleksey Arkhipenko <arhipenko1990@inbox.ru>
2021-11-29 14:16:04 +0300

    Code

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int count = 21309;
+        int price = 20;
+        int bonus = count / price;
+        System.out.println("ПОТРАЧЕННАЯ СУММА: " + count);
+        System.out.println("ПОЛУЧЕННЫЕ БОНУСЫ: " + bonus);
+    }
+
+}
